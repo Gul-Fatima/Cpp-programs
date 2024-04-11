874df8e228715e50b514ebdaa328ea6b890f2b72
GUL FATIMA <143262038+Gul-Fatima@users.noreply.github.com>
2024-04-11 05:52:54 -0700

    Create Area_of_rectangle.cpp

---
+++ Input,Output in cpp/Area_of_rectangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h> 
+
+using namespace std;
+
+int main() {
+    cout << "Gul Fatima, SE-23007" << endl;
+    int length, breadth;
+    cout << "Enter the length of rectangle : ";
+    cin >> length;
+    cout << "Enter the breadth of rectangle : ";
+    cin >> breadth;
+    cout << "The area of rectangle having length " << length << " units and breadth " << breadth << " units is " << length * breadth <<" square units.";
+    _getch(); 
+    return 0;
+}
+
+
