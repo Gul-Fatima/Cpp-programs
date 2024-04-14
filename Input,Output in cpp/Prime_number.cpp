8b6160fb3206cbf19d152caa76cfee9c00d03722
GUL FATIMA <143262038+Gul-Fatima@users.noreply.github.com>
2024-04-14 06:47:45 -0700

    Create Prime_number.cpp

---
+++ Input,Output in cpp/Prime_number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    cout << "Gul Fatima, SE-23007" << endl;
+    int number;
+    cout << "Enter a number to check if it's prime: ";
+    cin >> number;
+
+    bool isPrime = true;
+    for (int i = 2; i < number; i++) {
+        if (number % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime && number > 1) {
+        cout << number << " is a prime number" << endl;
+    }
+    else {
+        cout << number << " is not a prime number" << endl;
+    }
+
+    _getch();
+    return 0;
+}
