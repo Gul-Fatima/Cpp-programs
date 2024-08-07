f9f005b562e866d47d290ad79f07ecc4376e2551
GUL FATIMA <143262038+Gul-Fatima@users.noreply.github.com>
2024-08-07 03:45:26 -0700

    Create UprightStarPyramid.cpp

---
+++ Pattern/UprightStarPyramid.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void main() {
+	int n = 5;
+	for (int i = n; i >=0; i--)
+	{
+		for (int j = 0; j <= n-i-1; j++)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j < 2*i+1; j++)
+		{
+			cout << "*";
+		}
+		
+		cout << endl;
+	}
+	
+	
+
+}
+
+
