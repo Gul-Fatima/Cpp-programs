cc2f519fb43dfa7a3daa846df0340bff24598c7c
GUL FATIMA <143262038+Gul-Fatima@users.noreply.github.com>
2024-08-07 03:19:11 -0700

    Create Box.cpp

---
+++ Pattern/Box.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+}
