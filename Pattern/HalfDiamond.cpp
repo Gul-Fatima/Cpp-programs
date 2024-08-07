18653bffba2735ac14bf01e0e109b0e8436bc935
GUL FATIMA <143262038+Gul-Fatima@users.noreply.github.com>
2024-08-07 03:49:20 -0700

    Create HalfDiamond.cpp

---
+++ Pattern/HalfDiamond.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void main() {
+	int n = 5;
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j <=i; j++)
+		{
+			cout << "*";
+		}
+
+		cout << endl;
+	}
+	for (int i = n; i >=0; i--)
+	{
+		for (int j = 0; j <=i; j++)
+		{
+			cout << "*";
+		}
+		
+		cout << endl;
+	}
+	
+	
+
+}
+
+
