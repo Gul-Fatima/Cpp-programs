8eb75f60260ff2391e98d173419cf0371e60b3ba
GUL FATIMA <143262038+Gul-Fatima@users.noreply.github.com>
2024-08-09 09:36:13 -0700

    Create BoxPrint.cpp

---
+++ Pattern/BoxPrint.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n = 5;
+	int start = 1;
+	for (int i = 0; i < n ; i++)
+	{
+		
+		for (int j = 0; j < n; j++)
+		{
+			if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+				cout << "*";
+							}
+			else {
+				cout << " ";
+			}
+		}
+		
+		cout << endl;
+	}
+}
